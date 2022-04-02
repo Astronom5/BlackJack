f0e3e7e2b51329e28042c3b2104622482f1592c8
Astronom5 <mateusz.pawelec@zhr.pl>
2022-04-03 00:59:51 +0200

    Started working on power method

--- objects.py
+++ objects.py
@@ -1,4 +1,3 @@
-from cgitb import handler
 from random import shuffle
 
 
@@ -6,11 +5,11 @@ class Card:
     def __init__(self,colour,value):
         self.colour = colour
         self.value = value
-        print(self.colour, self.value)
     def __str__(self):
         return f'{self.colour},{self.value}'
     def __repr__(self):
         return f'{self.colour} {self.value}'
+
 
 class Deck:
     def __init__(self,list_of_colours,list_of_values):
@@ -22,13 +21,14 @@ class Deck:
         shuffle(self.deck)
 
 
-#### Do napisania player, krupier (przewidziane do rozwoju w multiplayer)
 class Player:
     def __init__(self,nick):
         self.nick = nick
         self.hand = Hand()
     def __repr__(self) -> str:
         return f'{self.nick}, {self.hand}'
+
+
 class Hand:
     cards = None
     def __init__(self):
@@ -39,24 +39,27 @@ class Hand:
     def __repr__(self):
         return f'{self.cards}'
     def power(self):
+        self.points = 0 # Optionally change names between power and points.
+        if self.cards != []:
+            for x in range(len(self.cards)):
+                print(self.cards[x])
+        else:
+            ####ERROR Exception needed        
         #Needed to be updated Rules : numbers -> number value, figures -> 10 Ace -> 1 or 11, 2 Aces ->21 (if at start)
-        pass
+            pass
 
 
-Ace = Card('Spades', 'A')
+class Game:
+    pass
 
-list_of_colours =['Spade','Heart','Diamond','Clover']
-list_of_values = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
+list_of_colours =['Spade','Heart','Diamond','Clover'] ### transfer it to Deck?
+list_of_values = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'] ### transfer it to Deck?
 deck = Deck(list_of_colours,list_of_values)
 print(deck.deck)
 deck.shuffle()
-print(Ace)
-my_hand = Hand()
-my_hand.draw(deck.deck,1)
-print(my_hand.cards)
-
 Me = Player('Astronom')
 Me.hand.draw(deck.deck,10)
 print(Me)
-print(len(deck.deck))
+Me.hand.power()
+
 

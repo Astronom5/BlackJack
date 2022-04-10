4080138627a5ef82c1cec500527f67fb5bb6b89b
Astronom5 <mateusz.pawelec@zhr.pl>
2022-04-10 16:22:45 +0200

    Working game alpha, started renaming files

--- objects.py
+++ objects.py
@@ -106,6 +106,7 @@ class Game:
 
 list_of_colours =['Spade','Heart','Diamond','Clover'] ### transfer it to Deck?
 list_of_values = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'] ### transfer it to Deck?
-game = Game(list_of_colours,list_of_colours,list_of_values,5)
+list_of_nicknames=['Tata', 'Mateusz']
+game = Game(list_of_nicknames,list_of_colours,list_of_values,5)
 game.play()
 

60df9233c8b281afde56529f35f4cabfd623cba7
Astronom5 <mateusz.pawelec@zhr.pl>
2022-04-12 17:36:11 +0200

    Added tests for card module

--- card_test.py
+++ card_test.py
@@ -1,7 +1,15 @@
+"""Tests for card module"""
 from card import Card
 
-def card_creation_test():
+def test_card_creation():
+    """Checks if card is properly created"""
     test_card = Card('spades','A')
     assert test_card.colour == 'spades'
+    assert test_card.value == 'A'
 
+def test_card_types():
+    """Checks if card's value or card's colour types are valid"""
+    test_card = Card('spades',2)
+    assert isinstance(test_card.colour, str)
+    assert isinstance(test_card.value, (str,int))
     

0b044d16f32291e6db28888b264c9c6b852e8af8
Astronom5 <mateusz.pawelec@zhr.pl>
2022-04-12 17:00:36 +0200

    Small code changes, started working on tests

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ from game import Game
 list_of_colours =['Spade','Heart','Diamond','Clover'] ### transfer it to Deck?
 list_of_values = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'] ### transfer it to Deck?
 list_of_nicknames=['Tata', 'Mateusz']
-game = Game(list_of_nicknames,list_of_colours,list_of_values,5)
+max_amount_of_players = 5
+game = Game(list_of_nicknames,list_of_colours,list_of_values,max_amount_of_players)
 game.play()

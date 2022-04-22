479a97936c1733188c17de511e8f95c70cec84b2
Astronom5 <mateusz.pawelec@zhr.pl>
2022-04-22 10:44:03 +0200

    Added missing docstring

--- hand_test.py
+++ hand_test.py
@@ -10,8 +10,10 @@ list_of_ace = ['A']
 
 
 def test_power_counting_ace():
+    """Tests if game properly counts 2 aces"""
     my_deck = Deck(list_of_colours,list_of_ace)
     my_hand = Hand()
     my_hand.draw(my_deck.deck,2)
     my_hand.power()
     assert my_hand.points == 21
+    
